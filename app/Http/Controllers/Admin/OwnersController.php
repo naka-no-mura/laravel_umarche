d2a175133ac9d413c161082a2318fd340b8090d0
naka-no-mura <nakanomura.ryu@gmail.com>
2021-09-27 13:09:18 +0900

    force-destroy

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -61,7 +61,10 @@ class OwnersController extends Controller
 
         return redirect()
         ->route('admin.owners.index')
-        ->with('message', 'オーナー登録を実施しました');
+        ->with([
+            'message' => 'オーナー登録を実施しました',
+            'status' => 'info'
+        ]);
     }
 
     /**
@@ -105,7 +108,10 @@ class OwnersController extends Controller
 
         return redirect()
         ->route('admin.owners.index')
-        ->with('message', 'ユーザー情報を更新しました');
+        ->with([
+            'message' => 'ユーザー情報を更新しました',
+            'status' => 'info'
+        ]);
     }
 
     /**
@@ -116,6 +122,23 @@ class OwnersController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Owner::findOrFail($id)->delete();
+
+        return redirect()
+        ->route('admin.owners.index')
+        ->with([
+            'message' => 'ユーザー情報を削除しました',
+            'status' => 'alert'
+        ]);
+    }
+
+    public function expiredOwnerIndex(){
+        $expiredOwners = Owner::onlyTrashed()->get();
+        return view('admin.expired-owners', compact('expiredOwners'));
+    }
+
+    public function expiredOwnerDestroy($id){
+        Owner::onlyTrashed()->findOrFail($id)->forceDelete();
+        return redirect()->route('admin.expired-owners.index');
     }
 }
