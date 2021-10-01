f114118d082f31f9704b524742398e46c82c162d
naka-no-mura <nakanomura.ryu@gmail.com>
2021-10-01 10:10:11 +0900

    mail

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -6,8 +6,11 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Stock;
-use Illuminate\Support\Facades\DB;
 use App\Models\PrimaryCategory;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\TestMail;
+use App\Jobs\SendThanksMail;
 
 class ItemController extends Controller
 {
@@ -30,6 +33,8 @@ class ItemController extends Controller
 
     public function index(Request $request)
     {
+        SendThanksMail::dispatch();
+
         $categories = PrimaryCategory::with('secondary')
         ->get();
 
